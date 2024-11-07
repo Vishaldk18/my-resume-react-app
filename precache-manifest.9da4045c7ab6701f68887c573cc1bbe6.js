62f5f505e1193fa87af1290307b1fd7a78977d81
Vishal Deepak Khairnar <vishaldgk18@gmail.com>
2024-11-07 23:35:15 +0530

    Updates

---
+++ precache-manifest.9da4045c7ab6701f68887c573cc1bbe6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7caf3997e84243636dfabe11e51a0e47",
+    "url": "/my-resume-app/index.html"
+  },
+  {
+    "revision": "2c22fe5c60a4506a5f08",
+    "url": "/my-resume-app/static/css/2.d88a60dc.chunk.css"
+  },
+  {
+    "revision": "ae8888eeecb0ec610ca9",
+    "url": "/my-resume-app/static/css/main.f1dfb855.chunk.css"
+  },
+  {
+    "revision": "2c22fe5c60a4506a5f08",
+    "url": "/my-resume-app/static/js/2.edea7e35.chunk.js"
+  },
+  {
+    "revision": "daedb6e9c1e75eb2181f2dde37df55f3",
+    "url": "/my-resume-app/static/js/2.edea7e35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae8888eeecb0ec610ca9",
+    "url": "/my-resume-app/static/js/main.c2788cae.chunk.js"
+  },
+  {
+    "revision": "de92074d8b2d73026876",
+    "url": "/my-resume-app/static/js/runtime-main.5faec48b.js"
+  },
+  {
+    "revision": "ca917141d028ebe6742d306483169f96",
+    "url": "/my-resume-app/static/media/cricketly.ca917141.jpg"
+  },
+  {
+    "revision": "aa5eca092787642bd7368af10768fdc0",
+    "url": "/my-resume-app/static/media/person1.aa5eca09.jpg"
+  },
+  {
+    "revision": "efc8a86a6ba1166d8fc7dc81249935f4",
+    "url": "/my-resume-app/static/media/video.efc8a86a.jpg"
+  },
+  {
+    "revision": "ad0b18432419ea38e51f3649bd7bca4f",
+    "url": "/my-resume-app/static/media/weathermate.ad0b1843.jpg"
+  }
+]);
